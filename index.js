462a9d0c228d07feb2bc92836d80a49690e95bd2
Max Ogden <max@maxogden.com>
2014-02-14 14:44:09 -0500

    1.0.0 - convert to streams2

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-var Transform = require('readable-stream').Transform;
-var util = require('util');
+var Transform = require('readable-stream').Transform
+var util = require('util')
 
-module.exports = MargaretBatcher;
-util.inherits(MargaretBatcher, Transform);
+module.exports = MargaretBatcher
+
+util.inherits(MargaretBatcher, Transform)
+
 function MargaretBatcher(limit) {
   if (!(this instanceof MargaretBatcher)) {
-    return new MargaretBatcher(limit);
+    return new MargaretBatcher(limit)
   }
-  Transform.call(this);
-  this.limit = limit || 4096;// 4KB, arbitrary
-  this.currentBatch = new Buffer(0);
-  this.size = 0;
+  Transform.call(this)
+  this.limit = limit || 4096 // 4KB, arbitrary
+  this.currentBatch = new Buffer(0)
+  this.size = 0
 }
+
 MargaretBatcher.prototype._transform = function(obj, _, cb) {
-  this.size += obj.length;
-  this.currentBatch = Buffer.concat([this.currentBatch, obj], this.size);
+  this.size += obj.length
+  this.currentBatch = Buffer.concat([this.currentBatch, obj], this.size)
   // keep batches under limit
   while (this.size >= this.limit) {
-    this.push(this.currentBatch.slice(0, this.limit));
-    this.currentBatch = this.currentBatch.slice(this.limit);
-    this.size = this.currentBatch.length;
+    this.push(this.currentBatch.slice(0, this.limit))
+    this.currentBatch = this.currentBatch.slice(this.limit)
+    this.size = this.currentBatch.length
   }
-  cb();
-};
+  cb()
+}
   
 MargaretBatcher.prototype._flush = function(cb) {
-  this.push(this.currentBatch);
-  cb();
-};
+  this.push(this.currentBatch)
+  cb()
+}
